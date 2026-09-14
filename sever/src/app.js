16a016f0e5f0fc16a18a3b4e4bc42b9590c2332a
Sneha Mishra <snehamishraa522@gmail.com>
2026-09-14 11:19:35 +0530

    Chat feature backend developed

--- sever/src/app.js
+++ sever/src/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
-// import chatRouter from "./routes/chat.routes.js";
+import chatRouter from "./routes/chat.routes.js";
+
 import morgan from "morgan";
 import cors from "cors";
 
@@ -13,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     credentials: true,
     methods: [ "GET", "POST", "PUT", "DELETE" ],
 }))
@@ -23,7 +24,8 @@ app.get("/", (req, res) => {
     res.json({ message: "Server is running" });
 });
 
+
 app.use("/api/auth", authRouter);
-// app.use("/api/chats", chatRouter);
+app.use("/api/chats", chatRouter);
 
 export default app;
--- sever/src/services/ai.service.js
+++ sever/src/services/ai.service.js
@@ -2,14 +2,73 @@
 
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
-const model = new ChatGoogleGenerativeAI({
-  model: "gemini-3.7-flash",
+import {ChatMistralAI} from  "@langchain/mistralai"
+
+import {HumanMessage, SystemMessage, AIMessage, AIMessageChunk} from "langchain"
+
+
+// Gemini model
+const geminiModel = new ChatGoogleGenerativeAI({
+  model: "gemini-3.5-flash-lite",
   apiKey: process.env.GEMINI_API_KEY
 });
 
-// export async function testAi(){
-//     model.invoke("What is the capital of India").then((response)=>{
-//         console.log(response.text)
-//     })
-// }
+
+// Mistral Model = for generating chat title
+// const mistralModel = new ChatMistralAI({
+//     model: "mistral-small-latest",
+//     apiKey: process.env.MISTRAL_API_KEY
+// })
+
+
+// Initialize model with automatic retries for HTTP 429 rate limits
+const mistralModel = new ChatMistralAI({
+  model: "open-mistral-7b",
+  apiKey: process.env.MISTRAL_API_KEY,
+  maxRetries: 5, 
+});
+
+
+export async function generateChatTitle(message) {
+
+    const response = await mistralModel.invoke([
+        new SystemMessage(`
+            You are a helpful assistant that generates concise and descriptive titles for chat conversations.
+            
+            User will provide you with the first message of a chat conversation, and you will generate a title that captures the essence of the conversation in 2-4 words. The title should be clear, relevant, and engaging, giving users a quick understanding of the chat's topic.    
+        `),
+        new HumanMessage(`
+            Generate a title for a chat conversation based on the following first message:
+            "${message}"
+            `)
+    ])
+
+    return response.text;
+
+}
+
+
+// Gemini model for response 
+export async function generateResponse(messages) {
+    console.log(messages)
+
+    const response = await agent.invoke({
+        messages: [
+            new SystemMessage(`
+                You are a helpful and precise assistant for answering questions.
+                If you don't know the answer, say you don't know. 
+                If the question requires up-to-date information, use the "searchInternet" tool to get the latest information from the internet and then answer based on the search results.
+            `),
+            ...(messages.map(msg => {
+                if (msg.role == "user") {
+                    return new HumanMessage(msg.content)
+                } else if (msg.role == "ai") {
+                    return new AIMessage(msg.content)
+                }
+            })) ]
+    });
+
+    return response.messages[ response.messages.length - 1 ].text;
+
+}
 
