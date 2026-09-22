90197481fdce371befcee7d28c821e69982978da
Sneha Mishra <snehamishraa522@gmail.com>
2026-09-22 23:57:43 +0530

    Adding render url

--- client/src/features/chat/service/chat.socket.js
+++ client/src/features/chat/service/chat.socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 
 export const initializeSocketConnection=()=>{
-    const socket = io("http://localhost:3000",{
+    const socket = io("https://seekora.onrender.com/",{
         withCredentials:true,
     })
 
--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -38,7 +38,7 @@ export async function register(req, res) {
                 <p>Hi ${username},</p>
                 <p>Thank you for registering at <strong>Perplexity</strong>. We're excited to have you on board!</p>
                 <p>Please verify your email address by clicking the link below:</p>
-                <a href="http://localhost:3000/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
+                <a href="https://seekora.onrender.com/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
                 <p>If you did not create an account, please ignore this email.</p>
                 <p>Best regards,<br>The Perplexity Team</p>
         `
@@ -174,7 +174,7 @@ export async function verifyEmail(req, res) {
             `
         <h1>Email Verified Successfully!</h1>
         <p>Your email has been verified. You can now log in to your account.</p>
-        <a href="http://localhost:3000/login">Go to Login</a>
+         <a href="https://seekora.onrender.com/login">Go to Login</a>
     `
 
         return res.send(html);
